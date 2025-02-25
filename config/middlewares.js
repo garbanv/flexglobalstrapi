397001aac15e5005cf4a305265aff4363d9c9dea
Alexei Garban <garban.valdeon@gmail.com>
2025-02-25 17:20:21 +0100

    remove unused plugin

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,17 +8,6 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'script-src': ['https://cdn.ckeditor.com'],
-          'connect-src': ['https://proxy-event.ckeditor.com']
-        },
-      },
-    },
-  },
+  'strapi::public'
+
 ];
